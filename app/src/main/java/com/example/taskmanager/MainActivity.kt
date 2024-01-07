769f1e9b1a357f691daeb2774486dbcc59fd8ddc
Shraddha <shraddhagunjal1037@gmail.com>
2024-01-07 22:58:09 +0530

    Finished app development

--- app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -1,15 +1,29 @@
 package com.example.taskmanager
 
 import android.os.Bundle
+import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.taskmanager.ui.theme.TaskManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +36,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    GreetingTaskManager(
+                        stringResource(R.string.all_tasks_completed) ,
+                        stringResource(R.string.nice_work)
+                    )
+                    //Greeting("Android")
                 }
             }
         }
@@ -30,17 +48,40 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun GreetingTaskManager(name: String,message: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.ic_task_completed)
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        Text(
+            text = name,
+            modifier = Modifier
+                .padding(
+                    top = 24.dp,
+                    bottom = 8.dp
+                ),
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = message,
+            fontSize = 16.sp
+
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TaskManagerTheme {
-        Greeting("Android")
+        //Greeting("Android")
     }
 }
